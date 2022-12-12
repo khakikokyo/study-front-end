92b761faeb231787bdcaa71218810b84462366b7
khakikokyo <phyeyoung90@gmail.com>
2022-12-13 03:49:05 +0900

    TailWind CSS 프레임워크를 사용하여 스타일링하기

--- react/react-todo-app/src/components/List.js
+++ react/react-todo-app/src/components/List.js
@@ -1,22 +1,5 @@
 function List({ todoData, setTodoData }) {
 
-  const btnStyle = {
-    color: "#fff",
-    border: "none",
-    padding: "5px 9px",
-    borderRadius: "50%",
-    cursor: "pointer",
-    float: "right"
-  };
-
-  const getStyle = (completed) => {
-    return {
-      padding: "15px",
-      borderBottom: "1px #ccc dotted",
-      textDecoration: completed ? "line-through" : "none"
-    };
-  };
-
   // 할 일 목록 삭제하기
   const handleClick = (id) => {
     let newTodoData = todoData.filter(data => data.id !== id);
@@ -37,18 +20,20 @@ function List({ todoData, setTodoData }) {
   return (
     <div>
       {todoData.map((data) => (
-        <div style={getStyle(data.completed)} key={data.id}>
-          <input
-            type="checkbox"
-            defaultChecked={false}
-            onChange={()=>{ handleCompleChange(data.id) }}
-          />
-          {data.title}
-          <button
-            style={btnStyle}
-            onClick={()=>{handleClick(data.id)}}>
-              x
-          </button>
+        <div key={data.id}>
+          <div className="flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded">
+            <div className="items-center">
+              <input
+                type="checkbox"
+                defaultChecked={false}
+                onChange={()=>{ handleCompleChange(data.id) }}
+              />{" "}
+              <span className={data.completed ? "line-through" : undefined}>{data.title}</span>
+            </div>
+            <div className="items-center">
+              <button onClick={()=>{handleClick(data.id)}}>x</button>
+            </div>
+          </div>
         </div>
       ))}
     </div>
