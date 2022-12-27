e8dc4b93da6471445b92614fc70cd213e2bde7b1
khakikokyo <phyeyoung90@gmail.com>
2022-12-28 02:56:06 +0900

    GET 요청 해보기

---
+++ nodejs/todo-app/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.listen(8080, function() {
+  console.log("listening on 8080");
+});
+
+app.get('/pet', function(request, response) {
+  response.send('펫용품 쇼핑 페이지입니다.');
+});
+
+app.get('/beauty', function(request, response) {
+  response.send('뷰티용품 쇼핑 페이지입니다.');
+});
