3a1fc295c197f3348c3aea5d3ba4018dd312b6bc
khakikokyo <phyeyoung90@gmail.com>
2022-12-28 17:02:08 +0900

    form 데이터를 서버로 전송(POST 요청)

--- nodejs/todo-app/server.js
+++ nodejs/todo-app/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 
 app.listen(8080, function() {
   console.log("listening on 8080");
@@ -19,4 +23,12 @@ app.get('/', function(request, response) {
 
 app.get('/write', function(request, response) {
   response.sendFile(__dirname + '/write.html');
+});
+
+// Post 요청
+app.post('/add', function(request, response) {
+  response.send('전송완료');
+  console.log(request.body.date);
+  console.log(request.body.title);
+  console.log(request.body);
 });
