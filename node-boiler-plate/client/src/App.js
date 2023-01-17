30503ab942e33aa4f5c1f92dab61860143acee9b
khakikokyo <phyeyoung90@gmail.com>
2023-01-17 16:32:52 +0900

    React Router Dom

--- node-boiler-plate/client/src/App.js
+++ node-boiler-plate/client/src/App.js
@@ -1,9 +1,17 @@
+import { Route, Routes } from 'react-router-dom'; 
 import './App.css';
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
 
 function App() {
   return (
     <div className="App">
-      Start
+      <Routes>
+        <Route index element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
     </div>
   );
 }
